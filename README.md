3ab84e3a48d37551f83e3c477e3f7d6a0fcc6c87
Sahil Maheshwari <maheshwari.sahil1@gmail.com>
2019-08-11 09:09:37 -0700

    Terraform output and remote state

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ud_terraform
 Terraform Repo
+
+# Generate Key Pair
+`ssh-keygen -f mykey`
+`ssh-keygen -f mykey.pub -m 'PEM' -e > mykey.pem`
+
+# terraform.tfvars
+AWS_ACCESS_KEY = "XXXX"
+AWS_SECRET_KEY = "YYYY"
---
+++ samples/demo-3_output_variables/vars.tf
@@ -0,0 +1,18 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-2 = "ami-05c1fa8df71875112"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
